fff576f21d6e65603ea518b0810912481d79a516
Jacson Santos <dev.jhacks@gamil.com>
2024-01-31 13:54:54 -0300

    Acrescento de informações ao README!

--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <?php
-$projetos = [
-	'ProjetoPessoaExemplo',
-	'ProjetoWeb_NGTI',
-	'napnePHP',
-	'ProjetoWeb_NGTI-qfv10',
-	'sorteio',
-	'Vercoes_Contatos'
-];
-$viewInfo = isset($_GET['viewInfo']) ? $_GET['viewInfo'] : 0;
+//liste seus projetos neste array!
+$projetos = ["projeto1","projeto2"];
+//variável contendo informações do servidor
+$info = isset($_GET['viewInfo']) ? $_GET['viewInfo'] : 0;
+//diretório a procurar a pasta zroot; "./" padrão
 $dir =  "./";
-if ($viewInfo == 1) :
+//verifica se foi passado algum parâmero do tipo get na uri com o valor 1
+// Se sim redireciona para a página mostrando informações do servidor
+if ($info == 1) :
 	echo "<a 
 	style='display:block;padding:9px;margin:.5em;color:#ff9;background-color:#abc;width:max-content;outline: none;text-decoration: none;' 
 	href='./'>Voltar
 	</a>";
 	phpinfo();
 else :
-	include_once './zroot/html/base.php';
+	include_once $dir."zroot/html/base.php";
 endif;
--- zroot/html/base.php
+++ zroot/html/base.php
@@ -7,22 +7,18 @@
 </head>
 
 <body>
+    <!--lista de nomes do gifs contidos na pasta imgs do zroot-->
     <?php $giffs = ['macaquinho', 'heroi', 'leao', 'mario', 'garota', 'gatinho', 'sonic']; ?>
     <header>
+        <!-- criação dos gifs dentro do header -->
         <?php foreach ($giffs as $gif) : ?>
             <img class="gif <?= $gif ?>" src="<?= $dir ?>zroot/imgs/<?= $gif ?>.gif" alt="GiF do <?= $gif ?>" srcset="">
         <?php endforeach ?>
     </header>
     <main class="container">
-        <?php if ($dir == './') : ?>
-            <div class="info">
-                <a href='?viewInfo=1'>Informações sobre o server</a>
-            </div>
-        <?php else : ?>
-            <div class="btnVoltar">
-                <a href='../'>Voltar</a>
-            </div>
-        <?php endif ?>
+        <div class="info">
+            <a href='?viewInfo=1'>Informações sobre o server</a>
+        </div>
         <h1><img src="<?= $dir ?>zroot/imgs/logo.png" alt="Meus projetos"></h1>
         <ul class="content">
             <?php
@@ -31,8 +27,9 @@
             }
             ?>
         </ul>
-        </main>
+    </main>
     <footer>
+        <!-- criação dos gifs dentro do footer -->
         <?php for ($i = (count($giffs) - 1); $i >= 0; $i--) : ?>
             <img class="gif <?= $giffs[$i] ?>" src="<?= $dir ?>zroot/imgs/<?= $giffs[$i] ?>.gif" alt="GiF do <?= $giffs[$i] ?>" srcset="">
         <?php endfor ?>
